e06ac1b4cb47fa7e30b637718b31fd73f275b1f0
Motasim Hussain <motasimhussain@gmail.com>
2014-05-17 23:08:03 +0500

    Voucher model finished

--- application/models/voucher_frm.php
+++ application/models/voucher_frm.php
@@ -6,12 +6,28 @@ class Voucher_frm extends CI_Model {
 		v_type;
 		$data = array(
 			'type' => $this->input->post('v_type'),
-			if($this->input->post('v_type') == 'sales')'s_serial' => $this->input->post('serial'),
+			if($this->input->post('v_type') == 'sales'){
+				's_serial';
+			} else{
+				'p_serial';
+			} => $this->input->post('serial'),
 			'inv_for' => $this->input->post('inv_for'),
 			'cmp_name' => $this->input->post('cmp_name'),
-			'dscr' => ($this->input->post("chq_num")),
+			'dscr' => if($this->input->post("method") == "cash"){
+				 $this->input->post('dscr'); 
+			}else{
+				"CH# ".$this->input->post("chq_num")." ".$this->input->post("dscr");
+			},
 			'tot_amnt' => $this->input->post('tot_amnt'),
 		);
+
+		$query = $this->db->insert('sp_records',$data);
+
+		if($query){
+			return ture;
+		}else{
+			return false;
+		}
 	}
 
 }
