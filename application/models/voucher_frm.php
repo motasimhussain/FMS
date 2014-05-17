b15aeaede7a8608ecf79d4b5adcf4a80f857942a
Motasim Hussain <motasimhussain@gmail.com>
2014-05-17 23:13:31 +0500

    fixes

--- application/models/voucher_frm.php
+++ application/models/voucher_frm.php
@@ -3,21 +3,25 @@
 class Voucher_frm extends CI_Model {
 
 	function add(){
-		v_type;
-		$data = array(
-			'type' => $this->input->post('v_type'),
-			if($this->input->post('v_type') == 'sales'){
-				's_serial';
+		
+		if($this->input->post('v_type') == 'sales'){
+				$v_type = 's_serial';
 			} else{
-				'p_serial';
-			} => $this->input->post('serial'),
-			'inv_for' => $this->input->post('inv_for'),
-			'cmp_name' => $this->input->post('cmp_name'),
-			'dscr' => if($this->input->post("method") == "cash"){
+				$v_type ='p_serial';
+			}
+
+		$dscr = if($this->input->post("method") == "cash"){
 				 $this->input->post('dscr'); 
 			}else{
 				"CH# ".$this->input->post("chq_num")." ".$this->input->post("dscr");
-			},
+			}
+
+		$data = array(
+			'type' => $this->input->post('v_type'),
+			$v_type => $this->input->post('serial'),
+			'inv_for' => $this->input->post('inv_for'),
+			'cmp_name' => $this->input->post('cmp_name'),
+			'dscr' => $dscr,
 			'tot_amnt' => $this->input->post('tot_amnt'),
 		);
 
